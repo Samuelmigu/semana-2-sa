1572edbbdf47bbcaeee0828bec7381e31846f9b4
Samuelmigu <samuelj.miguel@gmail.com>
2023-09-28 13:10:32 +0200

    Create energiacinetica.py

---
+++ energiacinetica.py
@@ -0,0 +1,4 @@
+masa = float(input("masa (kg): "))
+velocidad = float(input("velocidad (m/s): "))
+energia_cinetica = (0.5*masa*velocidad*velocidad)
+print ("La energia cinética es: ", energia_cinetica)
